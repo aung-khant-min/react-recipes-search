4c515036bf70ef3a80ebf5c13343c192f9da56f6
aung-khant-min <60535933+aung-khant-min@users.noreply.github.com>
2020-04-17 00:02:20 +0630

    Updates

---
+++ precache-manifest.5aab1c2eeadf3c4d60a57ee947109956.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823c0f524c3e09b3beb84852273da27a",
+    "url": "/react-recipes-search/index.html"
+  },
+  {
+    "revision": "d9ded735fc24ee34e24d",
+    "url": "/react-recipes-search/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "c41638a4272ac2973cab",
+    "url": "/react-recipes-search/static/css/main.a3a52f1f.chunk.css"
+  },
+  {
+    "revision": "d9ded735fc24ee34e24d",
+    "url": "/react-recipes-search/static/js/2.2716f200.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-recipes-search/static/js/2.2716f200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c41638a4272ac2973cab",
+    "url": "/react-recipes-search/static/js/main.665dec4b.chunk.js"
+  },
+  {
+    "revision": "d14e9199fefc94fe732a",
+    "url": "/react-recipes-search/static/js/runtime-main.19f584fe.js"
+  }
+]);
