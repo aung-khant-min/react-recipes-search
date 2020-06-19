1b5854d48d9139ab5611dcff81dd722ac76f5bec
aung-khant-min <aungkhantmin@ucsm.edu.mm>
2020-06-19 20:56:50 +0630

    Updates

---
+++ precache-manifest.438d3daafa082b4c40ca6839da0d1b77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba8c66bbdc736ac3275658a3cf51399",
+    "url": "/react-recipes-search/index.html"
+  },
+  {
+    "revision": "64c231e8826e974fa062",
+    "url": "/react-recipes-search/static/css/main.0d40fc5b.chunk.css"
+  },
+  {
+    "revision": "20be69b9f40154621ca6",
+    "url": "/react-recipes-search/static/js/2.1e13b05b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-recipes-search/static/js/2.1e13b05b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64c231e8826e974fa062",
+    "url": "/react-recipes-search/static/js/main.8508f7e3.chunk.js"
+  },
+  {
+    "revision": "d14e9199fefc94fe732a",
+    "url": "/react-recipes-search/static/js/runtime-main.19f584fe.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-recipes-search/precache-manifest.5aab1c2eeadf3c4d60a57ee947109956.js"
+  "/react-recipes-search/precache-manifest.438d3daafa082b4c40ca6839da0d1b77.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-recipes-search/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
